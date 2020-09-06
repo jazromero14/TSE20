7e9d8f411e141141e6144c3dc4d5ba7920bcf148
Jazmin Romero <ra190899@alumno.udb.edu.sv>
2020-09-06 16:14:32 -0600

    filtro

--- app/src/main/java/com/udbstudents/tseapp/adapters/MyAdapter.kt
+++ app/src/main/java/com/udbstudents/tseapp/adapters/MyAdapter.kt
@@ -3,13 +3,26 @@ package com.udbstudents.tseapp.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.udbstudents.tseapp.R
 import com.udbstudents.tseapp.models.ActaAndMunicipio
+import java.util.*
+import kotlin.collections.ArrayList
 
-class MyAdapter(private val events: List<ActaAndMunicipio>) :
+class MyAdapter(private val events: MutableList<ActaAndMunicipio>) :
+
+
+
+
+
     RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+
+    private var emptyList : MutableList<ActaAndMunicipio> = mutableListOf()
+    private var emptylistFull: MutableList<ActaAndMunicipio> = mutableListOf()
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.ViewHolder {
         return ViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.item_event, parent, false)
@@ -20,6 +33,39 @@ class MyAdapter(private val events: List<ActaAndMunicipio>) :
         return events.size
     }
 
+    fun getFilter(): Filter? {
+        return exampleFilter
+    }
+
+    private val exampleFilter: Filter = object : Filter() {
+        override fun performFiltering(constraint: CharSequence): FilterResults {
+            val filteredList: MutableList<ActaAndMunicipio> = ArrayList()
+            if (constraint == null || constraint.length == 0) {
+                filteredList.addAll(emptylistFull)
+            } else {
+                val filterPattern =
+                    constraint.toString().toLowerCase(Locale.ROOT).trim { it <= ' ' }
+                for (item in emptylistFull) {
+                    if (item.municipio.nombre!!.toLowerCase(Locale.ROOT).contains(filterPattern)) {
+                        filteredList.add(item)
+                    }
+                }
+            }
+            val results = FilterResults()
+            results.values = filteredList
+            return results
+        }
+
+        override fun publishResults(
+            constraint: CharSequence,
+            results: FilterResults
+        ) {
+
+            events.clear()
+            events.addAll(results.values as MutableList<ActaAndMunicipio>)
+            notifyDataSetChanged()
+        }
+    }
     override fun onBindViewHolder(holder: MyAdapter.ViewHolder, position: Int) {
         val event = events[position]
 
--- app/src/main/java/com/udbstudents/tseapp/views/PrincipalActivity.kt
+++ app/src/main/java/com/udbstudents/tseapp/views/PrincipalActivity.kt
@@ -6,50 +6,111 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.WindowManager
+import android.view.inputmethod.EditorInfo
+import androidx.appcompat.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.udbstudents.tseapp.R
-import com.udbstudents.tseapp.models.Departamento
+import com.udbstudents.tseapp.adapters.MyAdapter
+import com.udbstudents.tseapp.models.ActaAndMunicipio
+import com.udbstudents.tseapp.models.Actas
+import com.udbstudents.tseapp.models.Municipio
 import kotlinx.android.synthetic.main.activity_principal.*
 
+
 class PrincipalActivity : AppCompatActivity() {
-
-
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
     private lateinit var mFirestore: FirebaseFirestore
     private lateinit var mAuth: FirebaseAuth
+    private var municipiosLista: MutableList<Municipio> = mutableListOf()
+    private var listaActasRecyclerView: MutableList<ActaAndMunicipio> = mutableListOf()
 
-    private var departamentoLista: MutableList<Departamento> = mutableListOf()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_principal)
+        setContentView(R.layout.activity_second )
         setSupportActionBar(mainToolbar)
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
 
         mAuth = FirebaseAuth.getInstance()
         mFirestore = FirebaseFirestore.getInstance()
 
-        mFirestore.collection("Departamentos").get()
+
+        mFirestore.collection("Municipios").get()
             .addOnCompleteListener {
                 if (it.isSuccessful) {
-                    val departamento = it.result!!.toObjects(Departamento::class.java)
-                    departamento.forEach { item ->
-                        departamentoLista.add(item)
+                    val municipio = it.result!!.toObjects(Municipio::class.java)
+                    municipio.forEach { value ->
+                        municipiosLista.add(value)
                     }
                 }
-                departamentoLista
+                getAllActas(municipiosLista)
             }
+    }
+
+    private fun getAllActas(municipios: MutableList<Municipio>) {
+        mFirestore.collection("Actas").get()
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val actasModel = it.result!!.toObjects(Actas::class.java)
+                    for (acta in actasModel) {
+                        for (municipio in municipios) {
+                            if (acta.idMunicipio == municipio.idMunicipio) {
+                                val actaIndex = actasModel.indexOf(acta)
+                                val actaAndMunicipio = ActaAndMunicipio(actaIndex, acta, municipio)
+                                listaActasRecyclerView.add(actaAndMunicipio)
+
+                            }
+                        }
+                    }
+                    inflateRecyclerView(listaActasRecyclerView)
+                    Log.e("FUNCIONA", "Listado de actas$actasModel")
+                } else {
+                    Log.e("NO FUNCIONA", "")
+                }
+            }
+    }
+
+    private fun inflateRecyclerView(list: MutableList<ActaAndMunicipio>) {
+        viewManager = androidx.recyclerview.widget.LinearLayoutManager(this)
+        viewAdapter = MyAdapter(list)
+        my_recycler_view.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+            viewAdapter.notifyDataSetChanged()
+        }
     }
 
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView: SearchView = searchItem.actionView as SearchView
+        searchView.imeOptions = EditorInfo.IME_ACTION_DONE;
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                (viewAdapter as MyAdapter).getFilter()?.filter(newText)
+                return false
+            }
+        })
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
+
+
             R.id.action_update -> {
 
                 true
